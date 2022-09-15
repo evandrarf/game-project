aa77116d4674a0e5e37469119ac16531348d599d
Evandra Raditya <evandraraditya049@gmail.com>
2022-09-15 10:11:39 +0700

    Menambahkan musik menu dan musik in game

--- script/Global.gd
+++ script/Global.gd
@@ -12,11 +12,16 @@ onready var lobby_game = $HUD/Control/LobbyGame
 onready var settings = $HUD/Control/Settings
 onready var nextButton = $Game/Menu/Control/NinePatchRect/TextureButton
 onready var XButton = $Game/Menu/Control/XButton
+onready var nyawa_scene = $Game/Nyawa
+onready var love = get_all_children(nyawa_scene)
+onready var home_screen_music = $HUD/AudioStreamPlayer
+onready var game_music = $Game/GameMusic
 
 var skor_num = 0
 var soal_terakhir = false
 var back_to_home = false
 var is_game_starting = false
+var nyawa = 3
 
 func _ready():
 	menu.hide()
@@ -32,11 +37,26 @@ func _ready():
 	var shortcut=ShortCut.new()
 	shortcut.set_shortcut(InputMap.get_action_list("next")[0])#I have configured in the InputMap an InputEventKey called selectWeapon1(it is the only one so we need the zero-th index)
 	nextButton.set_shortcut(shortcut)
+	nyawa = 3
+	home_screen_music.play()
 
 func _process(delta):
 	if(is_game_starting and Input.is_action_pressed("ui_cancel")) :
 		pause_game()
-		
+	match nyawa:
+		2:
+			love[2].hide()
+		1: 
+			love[1].hide()
+		0:
+			love[0].hide()
+			menu.show()
+			#get_tree().paused = true
+			back_to_home = true
+			menu_text.text = "Game Over"
+			menu_button_text.text = "Back"
+			
+
 		
 
 func start_again():
@@ -69,6 +89,7 @@ func _on_Asteroid_area_entered(area, index):
 			skor.text = str(skor_num)
 		else :
 			menu_text.text = "Jawaban Anda Salah"
+			nyawa -= 1
 	else :
 		if(index == arr_soal[nomor_soal]["jawaban"]):
 			skor_num += 1
@@ -86,6 +107,9 @@ func _on_TextureButton_pressed():
 	if(back_to_home):
 		hud.show()
 		game.hide()
+		nyawa = 3
+		game_music.stop()
+		home_screen_music.play()
 	else :
 		randomize()
 		soal.position_index = int(round(rand_range(0, 10)))
@@ -101,6 +125,12 @@ func _on_StartButton_pressed():
 	game.show()
 	is_game_starting = true
 	XButton.hide()
+	nyawa = 3
+	game_music.play()
+	home_screen_music.stop()
+	
+	for i in love:
+		i.show()
 
 
 func start_game() :
@@ -112,6 +142,8 @@ func start_game() :
 	skor.text = str(skor_num)
 	is_game_starting = true
 	back_to_home = false
+	home_screen_music.play()
+	game_music.stop()
 
 
 func _on_SettingsButton_pressed():
@@ -129,3 +161,10 @@ func _on_SettingsButton_pressed():
 
 func _on_XButton_pressed():
 	start_again()
+
+func get_all_children(in_node,arr:=[]):
+	arr.push_back(in_node)
+	#for child in in_node.get_children():
+		#arr = get_all_children(child,arr)
+	arr = in_node.get_children()
+	return arr
--- script/Rocket.gd
+++ script/Rocket.gd
@@ -3,6 +3,7 @@ extends Sprite
 
 export var speed = 200
 var screen_size 
+onready var nos_animation = $Control2
 
 enum {
 	UP
@@ -18,36 +19,49 @@ func _ready():
 
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
+	nos_animation.hide()
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 		state = RIGHT
+		nos_animation.show()
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
 		state = LEFT
+		nos_animation.show()
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
 		state = DOWN
+		nos_animation.show()
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
 		state = UP
+		nos_animation.show()
 		
 	match state:
 		UP:
 			rotate(0)
 			flip_v = false
 			frame = 0
+			nos_animation.rect_position = Vector2(-36.4, 71.172)
+			nos_animation.rect_rotation = 0
 		DOWN :
 			rotate(0)
 			flip_v = true
 			frame = 0
+			nos_animation.rect_position = Vector2(37, -70.019)
+			nos_animation.rect_rotation = 180
 		RIGHT :
 			flip_v = false
 			flip_h = false
 			frame = 1
+			nos_animation.rect_position = Vector2(-80.713, -41.356)
+			nos_animation.rect_rotation = 90
 		LEFT :
 			flip_v = false
 			frame =  1
 			flip_h = true
+			nos_animation.rect_position = Vector2(86.56, 25.968)
+			nos_animation.rect_rotation = 270
 	
 	if(frame == 0):
 		$Control.rect_rotation = 0
